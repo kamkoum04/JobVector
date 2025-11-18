30ea57d75f56b7dc993b971b3086eb7e115f55e5
kamkoum04 <hamza.kamkoum@tek-up.de>
2025-11-18 20:11:57 +0100

    terraform update

--- aws-eks-deployment/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-s3-bucket-name"
-    key            = "terraform/state"
-    region         = "your-aws-region"
-    dynamodb_table = "your-dynamodb-table-name"
-    encrypt        = true
-  }
-}
